51fe6f42f5e6f872805b71ef33e4d5a54b385f5a
Kevin Wood <kevin.wood75@gmail.com>
2021-11-22 17:32:06 -0500

    initial commit

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World & Kevin.  How are you doing!")
+    return HttpResponse("Hello, World & Kevin.  Whats new today!")
